410b0154075a9ff558724944ac36695c42e4b2bd
eyaadh <eyaadh@gmail.com>
2022-04-07 13:56:51 +0500

    Initial Commit

---
+++ QuranTharujama/QuranTharujamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranTharujamaApp.swift
+//  QuranTharujama
+//
+//  Created by Ahmed Iyad on 7-4-22.
+//
+
+import SwiftUI
+
+@main
+struct QuranTharujamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
