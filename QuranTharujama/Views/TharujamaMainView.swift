a813238875076351e2f2c928a6f6d8460d07e2cf
eyaadh <eyaadh@gmail.com>
2022-04-07 14:02:30 +0500

    initial inclusion of data project setup and folder structuring

---
+++ QuranTharujama/Views/TharujamaMainView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuranTharujama
+//
+//  Created by Ahmed Iyad on 7-4-22.
+//
+
+import SwiftUI
+
+struct TharujamaMainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct TPP: PreviewProvider {
+    static var previews: some View {
+        TharujamaMainView()
+    }
+}
